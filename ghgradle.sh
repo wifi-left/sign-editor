48b2c20067a9344265aadf11bc9626cca2dd1ff3
wifi-left <ysy1679682327@outlook.com>
2026-05-24 15:14:27 +0800

    Change gradle url

---
+++ ghgradle.sh
@@ -0,0 +1,4 @@
+chmod +x ./gradlew
+chmod +rwx gradle/wrapper/gradle-wrapper-forgithub.properties
+chmod +rwx gradle/wrapper/gradle-wrapper.properties
+cp -f gradle/wrapper/gradle-wrapper-forgithub.properties gradle/wrapper/gradle-wrapper.properties
